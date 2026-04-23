bae0c1f969f7ee306fdf23e60520f5f184df6388
DOMOINA <domoinaprincia2005@gmail.com>
2026-04-24 04:25:36 +0800

    Fix: Amélioration Service Worker

--- public/sw.js
+++ public/sw.js
@@ -5,18 +5,44 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
-// Installation : met en cache les fichiers
+// Installation
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
   );
+  self.skipWaiting();
 });
 
-// Récupération : sert les fichiers depuis le cache
+// Activation - nettoie l'ancien cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Ancien cache supprimé:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Récupération
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
